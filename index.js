4fc2071f2bffae6e53b32c72b19b9cb9d1628ac1
Ni Kadek Puja Ari Pertiwi <95269918+pujaari@users.noreply.github.com>
2024-04-17 12:49:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo Broo PPL ni Broo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+// Fungsi untuk menghitung IPK
+function hitungIPK(nilai) {
+    var totalBobot = 0;
+    var totalSKS = 0;
+    
+    // Loop melalui setiap nilai
+    for (var i = 0; i < nilai.length; i++) {
+        totalBobot += nilai[i].nilai * nilai[i].sks;
+        totalSKS += nilai[i].sks;
+    }
+    
+    // Menghitung IPK
+    var ipk = totalBobot / totalSKS;
+    
+    return ipk;
+}
+
+// Data dummy nilai mahasiswa
+var nilaiMahasiswa = [
+    { matkul: "Matematika", nilai: 3.5, sks: 4 },
+    { matkul: "Fisika", nilai: 3.2, sks: 3 },
+    { matkul: "Kimia", nilai: 3.8, sks: 3 },
+    { matkul: "Biologi", nilai: 3.6, sks: 4 },
+    { matkul: "Bahasa Inggris", nilai: 3.9, sks: 3 }
+];
+
+// Memanggil fungsi untuk menghitung IPK
+var ipkMahasiswa = hitungIPK(nilaiMahasiswa);
+
+// Menampilkan hasil IPK
+console.log("IPK Mahasiswa: " + ipkMahasiswa.toFixed(2));
